6249f8a2988dfbd856a83616555fd930544c0e89
uadhav goyal <uadhavgoyal14@gmail.com>
2026-06-15 15:58:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function scrollToSection(id){
+    document
+    .getElementById(id)
+    .scrollIntoView({
+        behavior:"smooth"
+    });
+}
+
+const topBtn =
+document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY > 300){
+        topBtn.style.display="block";
+    }
+    else{
+        topBtn.style.display="none";
+    }
+});
+
+topBtn.addEventListener("click",()=>{
+
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
+});
+
